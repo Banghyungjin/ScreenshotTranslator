87781415507d6f865a5bb014c08f848645bb16a3
HyungJin_Bang <37135305+Banghyungjin@users.noreply.github.com>
2022-05-04 23:14:37 +0900

    2022_05_04_001
    저장한 마우스 좌표에 해당하는 컴퓨터 화면 부분 스크린샷 캡쳐기능 추가

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ import configparser
 import time
 import AppKit
 import os
+import pyautogui
 
 
-def config_generator():
+def config_generator():  # 설정파일 만드는 메소드
     # 설정파일 만들기
     config_parser = configparser.ConfigParser()
     now = time.localtime()
@@ -41,11 +42,11 @@ def config_generator():
         os.mkdir(path)
 
 
-def on_click(x, y, button, pressed):  # 마우스 클릭 좌표 받아오기
+def on_click(x, y, button, pressed):  # 마우스 클릭 x, y 좌표 받아오기
     config_parser = configparser.ConfigParser()
     config_parser.read('config.ini')
-    if button.left and pressed:
-        with open('config.ini', 'w', encoding='utf-8') as configfile:
+    if button.left and pressed:  # 마우스 왼쪽 버튼 클릭 확인
+        with open('config.ini', 'w', encoding='utf-8') as configfile:  # config 파일 열고 좌표 적어 넣기
             if config_parser['mouse_counter']['value'] == '0':
                 config_parser.set('screensize', 'axisX1', str(x))
                 config_parser.set('screensize', 'axisY1', str(y))
@@ -58,15 +59,28 @@ def on_click(x, y, button, pressed):  # 마우스 클릭 좌표 받아오기
                 print(config_parser['screensize']['axisX1'], config_parser['screensize']['axisY1'])
                 print(config_parser['screensize']['axisX2'], config_parser['screensize']['axisY2'])
                 listener.stop()
+                now = time.localtime()
+                pyautogui.screenshot(os.path.join(os.getcwd(),
+                                                  config_parser['directory']['directory'],
+                                                  "%04d-%02d-%02d_%02d:%02d:%02d.png" % \
+                                                  (now.tm_year, now.tm_mon, now.tm_mday, now.tm_hour, now.tm_min,
+                                                   now.tm_sec)
+                                                  ),
+                                     region=(float(config_parser['screensize']['axisX1']) * 2,
+                                             float(config_parser['screensize']['axisY1']) * 2,
+                                             (float(config_parser['screensize']['axisX2']) - float(
+                                                 config_parser['screensize']['axisX1'])) * 2,
+                                             (float(config_parser['screensize']['axisY2']) - float(
+                                                 config_parser['screensize']['axisY1'])) * 2))
             config_parser.write(configfile)
         configfile.close()
 
 
-def coord_arrange(config_parser):   # 받아온 x, y 좌표를 크기 순으로 재배열
-    if config_parser['screensize']['axisX1'] > config_parser['screensize']['axisX2']:
+def coord_arrange(config_parser):  # 받아온 x, y 좌표를 크기 순으로 재배열
+    if float(config_parser['screensize']['axisX1']) > float(config_parser['screensize']['axisX2']):
         config_parser['screensize']['axisX1'], config_parser['screensize']['axisX2'] = \
             config_parser['screensize']['axisX2'], config_parser['screensize']['axisX1']
-    if config_parser['screensize']['axisY1'] > config_parser['screensize']['axisY2']:
+    if float(config_parser['screensize']['axisY1']) > float(config_parser['screensize']['axisY2']):
         config_parser['screensize']['axisY1'], config_parser['screensize']['axisY2'] = \
             config_parser['screensize']['axisY2'], config_parser['screensize']['axisY1']
 
